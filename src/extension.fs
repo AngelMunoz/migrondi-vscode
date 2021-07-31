d1e91b969a89220e09e3fbc40f5e2aa0110d8a6a
Angel D. Munoz <scythes.lol@gmail.com>
2021-07-31 00:01:11 -0500

    Initial commit

---
+++ src/extension.fs
@@ -0,0 +1,29 @@
+﻿[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Extension
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import.vscode
+open Ionide.VSCode.Helpers
+open global.Node.ChildProcess
+
+let activate (context: ExtensionContext) : unit =
+    printfn "Activating Fable extension"
+
+    let showMessage () =
+        Fable.Import.vscode.window.showInformationMessage ("Hello World!")
+
+    let disposable =
+        Fable.Import.vscode.commands.registerCommand (
+            "fsharp-fable-sample.helloWorld",
+            (fun _ -> showMessage () :> obj)
+        )
+
+    context.subscriptions.Add disposable
+
+let deactivate (disposables: Disposable []) =
+    printfn "Deactivating Fable extension"
+
+    disposables
+    |> Array.iter (fun d -> d.dispose () |> ignore)
