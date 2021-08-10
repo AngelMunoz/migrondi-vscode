f967e1f80b9e9519f77c9843bc112b65dc11e5f4
Angel D. Munoz <scythes.lol@gmail.com>
2021-08-09 23:34:18 -0500

    add migrondi interop fn

--- src/interop.ts
+++ src/interop.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
+import { spawn } from 'child_process';
 
 import { access, mkdir, } from 'fs/promises';
 import { Extract } from 'unzipper';
 
+import { ExtensionContext, OutputChannel, WorkspaceFolder } from 'vscode';
 import * as vscode from 'vscode';
 
-export async function getOrCreatePath(context: vscode.ExtensionContext, channel: vscode.OutputChannel) {
+export async function getOrCreatePath(context: ExtensionContext, channel: OutputChannel) {
     const path = context.globalStorageUri.fsPath;
     try {
         channel.appendLine(`Migrondi: checking "${path}" exists`);
@@ -36,4 +38,38 @@ export async function downloadAndExtract(url: string, extractTo: string) {
                 resolve(undefined);
             });
     });
+}
+
+export function getMigrondiExecFn(getExecPath: (context: ExtensionContext) => string, getCWD: (workspaceFolders: readonly WorkspaceFolder[] | undefined) => string) {
+    return async (args: [context: ExtensionContext, cmdArgs: readonly string[]]) => {
+        const [context, cmdArgs] = args;
+        const binFilePath = getExecPath(context);
+        const cwd = getCWD(vscode.workspace.workspaceFolders);
+        const process = spawn(`${binFilePath}`, cmdArgs, { cwd });
+        const stdout = [];
+        const stderr = [];
+        if (process.stdout) {
+            for await (const chunk of process.stdout) {
+                const chunkLines = chunk?.toString?.().trim?.().replace?.(/(?:\r\n|\r|\n)/g, ",");
+                stdout.push(chunkLines);
+            }
+        }
+        if (process.stderr) {
+            for await (const chunk of process.stderr) {
+                const chunkLines = chunk?.toString?.();
+                stderr.push(chunkLines);
+            }
+        }
+        const stdoutResult = `[${stdout.filter(x => !!(x.trim())).join(",")}]`;
+        const stderrResult = stderr.join("");
+
+        if (stderr.length > 0) {
+            return Promise.reject(stderrResult);
+        }
+        try {
+            return JSON.parse(stdoutResult);
+        } catch (error) {
+            return Promise.reject(error.message);
+        }
+    };
 }
